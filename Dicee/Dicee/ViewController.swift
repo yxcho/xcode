89de5ed948c6254a1a6c8860526a12f7aecba1d2
Yangxuan <yangxuan.1996@gmail.com>
2018-08-25 11:11:18 +1000

    dicee

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Yang Xuan on 25/8/18.
+//  Copyright © 2018 YX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
